818928b0931ffd4bd567735b76720764d7dcf603
azizamari <azizamari2003@gmail.com>
2020-12-31 17:38:11 +0100

    countries

--- app.js
+++ app.js
@@ -30,3 +30,29 @@ new Glider(document.querySelector('.glider'), {
     }
   ]
 });
+
+new Glider(document.querySelector('.glider2'), {
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: '.dots2',
+  responsive: [
+    {
+      breakpoint: 775,
+      settings: {
+        slidesToShow: 'auto',
+        slidesToScroll: 'auto',
+        itemWidth: 150,
+        duration: 0.25
+      }
+    },{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    }
+  ]
+});
